5ca1205d006cfa2267660f45f40ebaea24b52d47
Maverick <mavseneris@gmail.com>
2024-02-18 16:35:23 +0800

    Revise paragraph on line57

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import whatsApp from "./assets/images/whatsapp.png";
 import mail from "./assets/images/mail.png";
 import shopee from "./assets/images/shopee.png";
 
-
-
 function App() {
   const [logoVisible, setLogoVisible] = useState(false);
 
@@ -39,38 +37,57 @@ function App() {
       <section className="icons">
         <div>
           <img className="icon" src={user} />
-          <p >
-          We provide comprehensive assistance, including technical support, customer service, consultancy, and training, tailored to your company's needs.
+          <p>
+            We provide comprehensive assistance, including technical support,
+            customer service, consultancy, and training, tailored to your
+            company's needs.
           </p>
         </div>
         <div>
           <img className="icon" src={list} />
           <p>
-          Our outsourcing services streamline processes like inventory management and order processing to boost efficiency and reduce costs.
+            Our outsourcing services streamline processes like inventory
+            management and order processing to boost efficiency and reduce
+            costs.
           </p>
         </div>
         <div>
           <img className="icon" src={stamp} />
           <p>
-          Our outsourcing services streamline processes like inventory management and order processing to boost efficiency and reduce costs.
+            Access competitively priced, high-quality products from reputable
+            suppliers, optimizing your budget allocation.
           </p>
         </div>
         <div>
           <img className="icon" src={stars} />
           <p>
-          Our commitment to quality service ensures we meet and exceed your requirements, fostering long-term partnerships based on trust and reliability.
+            Our commitment to quality service ensures we meet and exceed your
+            requirements, fostering long-term partnerships based on trust and
+            reliability.
           </p>
         </div>
       </section>
       <section className="contacts">
-        <p className="contact-text">Contact us to learn more about our porducts and sercives</p>
+        <p className="contact-text">
+          Contact us to learn more about our porducts and sercives
+        </p>
         <div className="contact-icons">
-          <a href="https://shopee.ph/1.twovarietyshop"><img src={shopee}/></a>
-          <a href="https://www.facebook.com/profile.php?id=61554743732118"><img src={fb}/></a>
-          <a href="mailto:1twovarietyshop@gmail.com"><img src={mail}/></a>
-          <a href="tel:+639910081757"><img src={whatsApp}/></a>
+          <a href="https://shopee.ph/1.twovarietyshop">
+            <img src={shopee} />
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61554743732118">
+            <img src={fb} />
+          </a>
+          <a href="mailto:1twovarietyshop@gmail.com">
+            <img src={mail} />
+          </a>
+          <a href="tel:+639910081757">
+            <img src={whatsApp} />
+          </a>
         </div>
-        <p className="contact-text"># 25 Boston Street, San Dionisio, Paranaque City</p>
+        <p className="contact-text">
+          # 25 Boston Street, San Dionisio, Paranaque City
+        </p>
       </section>
     </main>
   );
